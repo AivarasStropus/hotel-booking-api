d3e4ad56ae040717d11b7f02c3c34718c5e0c926
unknown <aivpus@gmail.com>
2023-07-19 20:22:53 +0300

    initial commit

---
+++ src/main/java/lt/codeacademy/hotelbooking/HotelbookingApplication.java
@@ -0,0 +1,15 @@
+package lt.codeacademy.hotelbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelbookingApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
+
---
+++ src/main/java/lt/codeacademy/hotelbooking/entities/UserInfo.java
@@ -0,0 +1,43 @@
+package lt.codeacademy.hotelbooking.entities;
+
+import lombok.*;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+    @PreUpdate
+    void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+}
